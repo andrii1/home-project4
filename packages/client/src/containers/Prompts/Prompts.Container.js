6f924d00dde38bd0b63de6fa30877ae5661022c9
Andrii Gorokhovskyi <andrii.gorokhovskyi@gmail.com>
2023-02-24 14:34:09 +0100

    categories

--- packages/client/src/containers/Prompts/Prompts.Container.js
+++ packages/client/src/containers/Prompts/Prompts.Container.js
@@ -1,61 +1,103 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
+import { apiURL } from '../../apiURL';
 import './Prompts.Style.css';
 
-import { Card } from '../../components/Card/Card.component';
 import { Pagination } from '../../components/Pagination/Pagination.component';
 
-const cards = [
-  'GPT prompts',
-  'Design prompts',
-  'Coding promtps',
-  'Marketing prompts',
-  'Visual prompts',
-  'Copywriting prompts',
-];
 export const Prompts = () => {
-  const cardItems = cards.map((card) => <Card title={card} />);
+  const [isLoading, setIsLoading] = useState(false);
+  const [prompts, setPrompts] = useState([]);
+  const [categories, setCategories] = useState([]);
+  const [topics, setTopics] = useState([]);
+  const [filteredTopics, setFilteredTopics] = useState('');
+  const [filteredCategories, setFilteredCategories] = useState([]);
+  useEffect(() => {
+    async function fetchPrompts() {
+      const url = `${apiURL()}/prompts/`;
+      const response = await fetch(url);
+      const promptsResponse = await response.json();
+      console.log('pr', promptsResponse);
+
+      if (filteredCategories.length > 0) {
+        const filteredPrompts = promptsResponse.filter((item) =>
+          filteredCategories.includes(item.category_id),
+        );
+        console.log('filterCategories', filteredCategories);
+        console.log('pr3', filteredPrompts);
+        setPrompts(filteredPrompts);
+      } else {
+        setPrompts(promptsResponse);
+      }
+    }
+    async function fetchCategories() {
+      const response = await fetch(`${apiURL()}/categories/`);
+      const categoriesResponse = await response.json();
+      setCategories(categoriesResponse);
+    }
+    async function fetchTopics() {
+      const response = await fetch(`${apiURL()}/topics/`);
+      const topicsResponse = await response.json();
+      setTopics(topicsResponse);
+    }
+    fetchPrompts();
+    fetchCategories();
+    fetchTopics();
+  }, [filteredCategories]);
+
+  const filterHandler = (event) => {
+    if (event.target.checked) {
+      setFilteredCategories([
+        ...filteredCategories,
+        parseInt(event.target.value, 10),
+      ]);
+    } else {
+      setFilteredCategories(
+        filteredCategories.filter(
+          (filterCategory) =>
+            filterCategory !== parseInt(event.target.value, 10),
+        ),
+      );
+    }
+    console.log(filteredCategories);
+  };
+  const promptsList = prompts.map((prompt) => (
+    <div key={prompt.id} className="row prompts-body">
+      <div className="col-1">{prompt.title}</div>
+      <div className="col-2">Text goes here</div>
+      <div className="col-3">{prompt.categoryTitle}</div>
+      <div className="col-4">{prompt.topicTitle}</div>
+      <div className="col-5">Rating</div>
+      <div className="col-6">👍 / 👎</div>
+      <div className="col-7">❤️</div>
+      <div className="col-8">fb</div>
+    </div>
+  ));
+  const categoriesList = categories.map((category) => (
+    <li key={category.id}>
+      <input type="checkbox" value={category.id} onChange={filterHandler} />
+      {category.title}
+    </li>
+  ));
+  const topicsList = topics.map((topic) => (
+    <li key={topic.id}>
+      <input type="checkbox" /> {topic.title}
+    </li>
+  ));
+
   return (
     <main>
       <h1 className="hero-header">Prompts</h1>
+
       <section className="container-prompts">
         <div className="prompts-filter">
           <div className="tab-filter">Categories</div>
           <div className="checkboxes">
-            <ul>
-              <li>
-                <input type="checkbox" /> GPT
-              </li>
-              <li>
-                <input type="checkbox" /> Coding
-              </li>
-              <li>
-                <input type="checkbox" /> Graphics
-              </li>
-              <li>
-                <input type="checkbox" /> Marketing
-              </li>
-              <li>
-                <input type="checkbox" /> Project Management
-              </li>
-            </ul>
+            <ul>{categoriesList}</ul>
           </div>
-          <div className="tab-filter">Tools</div>
+          <div className="tab-filter">Topics</div>
           <div className="checkboxes">
-            <ul>
-              <li>
-                <input type="checkbox" /> ChatGPT
-              </li>
-              <li>
-                <input type="checkbox" /> Dream by Wombo
-              </li>
-              <li>
-                <input type="checkbox" /> Tool #3
-              </li>
-              <li>
-                <input type="checkbox" /> Tool #4
-              </li>
-            </ul>
+            <ul>{topicsList}</ul>
           </div>
         </div>
         <div className="prompts-table">
@@ -63,20 +105,13 @@ export const Prompts = () => {
             <div className="col-1">Prompt</div>
             <div className="col-2">Description</div>
             <div className="col-3">Category</div>
-            <div className="col-4">Rating</div>
-            <div className="col-5">Helpful?</div>
-            <div className="col-6">Favorite</div>
-            <div className="col-7">Share</div>
+            <div className="col-4">Topic</div>
+            <div className="col-5">Rating</div>
+            <div className="col-6">Helpful?</div>
+            <div className="col-7">Bookmark</div>
+            <div className="col-8">Share</div>
           </div>
-          <div className="row prompts-body">
-            <div className="col-1">Prompt#1</div>
-            <div className="col-2">Text goes here</div>
-            <div className="col-3">GPT</div>
-            <div className="col-4">2</div>
-            <div className="col-5">👍 / 👎</div>
-            <div className="col-6">❤️</div>
-            <div className="col-7">fb</div>
-          </div>
+          {promptsList}
         </div>
       </section>
       <Pagination />
