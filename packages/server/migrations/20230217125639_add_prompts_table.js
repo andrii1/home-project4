0d0df865e59b7fe05a0abdff79fac831ec62da41
Andrii Gorokhovskyi <andrii.gorokhovskyi@gmail.com>
2023-05-11 11:34:16 +0200

    build errors

--- packages/server/migrations/20230217125639_add_prompts_table.js
+++ packages/server/migrations/20230217125639_add_prompts_table.js
@@ -6,8 +6,8 @@ exports.up = function (knex) {
   return knex.schema.createTable('prompts', (table) => {
     table.increments();
     table.text('title').notNullable();
-    table.text('description').notNullable();
-    table.decimal('rating').notNullable();
+    table.text('description').nullable();
+    table.decimal('rating').nullable();
     table.integer('topic_id').unsigned();
     table.foreign('topic_id').references('id').inTable('topics');
     table.datetime('created_at', { precision: 6 }).defaultTo(knex.fn.now(6));
