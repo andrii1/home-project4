64d065cea20a3439bfc46d17fad8c7f4a78232ae
Andrii Gorokhovskyi <andrii.gorokhovskyi@gmail.com>
2023-05-12 23:00:08 +0200

    update

--- packages/server/knexfile.js
+++ packages/server/knexfile.js
@@ -18,13 +18,7 @@ module.exports = {
   },
   production: {
     client: 'postgresql',
-    connection: {
-      host: process.env.MYSQL_HOST,
-      port: process.env.MYSQL_PORT,
-      user: process.env.MYSQL_USER,
-      password: process.env.MYSQL_PASSWORD,
-      database: process.env.MYSQL_DATABASE,
-    },
+    connection: process.env.DATABASE_URL,
     pool: { min: 0, max: 7 },
     seeds: {
       directory: path.join(__dirname, '/seeds/production'),
