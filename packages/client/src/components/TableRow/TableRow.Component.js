8db25a26e4b441c27786dd9d6a8089f61c39d42d
Andrii Gorokhovskyi <andrii.gorokhovskyi@gmail.com>
2023-09-27 20:36:22 +0200

    update

--- packages/client/src/components/TableRow/TableRow.Component.js
+++ packages/client/src/components/TableRow/TableRow.Component.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import './PromptRow.Style.css';
+import './TableRow.Style.css';
 import iconCopy from '../../assets/images/icons8-copy-24.png';
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
