4f52cf8960e6fa3cc55ff5c42b4a3d1049c6957d
Andrii Gorokhovskyi <andrii.gorokhovskyi@gmail.com>
2024-09-12 11:24:55 +0200

    migrations

---
+++ packages/server/migrations/20240912092330_rename_searchTerms_table.js
@@ -0,0 +1,15 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.schema.renameTable('searchTerms', 'searches');
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return knex.schema.renameTable('searches', 'searchTerms');
+};
