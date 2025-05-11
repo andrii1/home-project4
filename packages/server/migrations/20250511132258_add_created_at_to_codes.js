a7027590f0facf6075287ab9c2792005507058d4
Andrii Gorokhovskyi <andrii.gorokhovskyi@gmail.com>
2025-05-11 15:23:51 +0200

    wip

---
+++ packages/server/migrations/20250511132258_add_created_at_to_codes.js
@@ -0,0 +1,19 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.schema.alterTable('codes', (table) => {
+    table.datetime('created_at', { precision: 6 }).defaultTo(knex.fn.now(6));
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return knex.schema.alterTable('codes', (table) => {
+    table.datetime('created_at', { precision: 6 }).defaultTo(knex.fn.now(6));
+  });
+};
