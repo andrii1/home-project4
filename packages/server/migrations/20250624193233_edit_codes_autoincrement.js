ad8e7c66b13086ce67f4aa528c5316c5ac515271
Andrii Gorokhovskyi <andrii.gorokhovskyi@gmail.com>
2025-06-24 21:36:24 +0200

    wip

---
+++ packages/server/migrations/20250624193233_edit_codes_autoincrement.js
@@ -0,0 +1,19 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = async function (knex) {
+  return knex.raw(`
+    ALTER TABLE codes MODIFY id INT UNSIGNED NOT NULL AUTO_INCREMENT
+  `);
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = async function (knex) {
+  return knex.raw(`
+    ALTER TABLE codes MODIFY id INT UNSIGNED NOT NULL
+  `);
+};
