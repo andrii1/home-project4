8b68313e7b79e9ee30ccc80b1cdf8ac8c59690f0
Andrii Gorokhovskyi <andrii.gorokhovskyi@gmail.com>
2025-06-30 18:54:40 +0200

    wip

--- packages/server/api/controllers/appsAppStore.controller.js
+++ packages/server/api/controllers/appsAppStore.controller.js
@@ -1,20 +1,9 @@
+/* eslint-disable import/no-extraneous-dependencies */
 /* TODO: This is an example controller to illustrate a server side controller.
 Can be deleted as soon as the first real controller is added. */
 
 const HttpError = require('../lib/utils/http-error');
 const fetch = require('node-fetch');
-
-// const getExampleResources = async () => {
-//   try {
-//     const url = 'https://itunes.apple.com/lookup?id=297606951';
-//     const response = await fetch(url);
-//     const jsonResponse = await response.json();
-//     console.log(jsonResponse);
-//     return jsonResponse;
-//   } catch (error) {
-//     return error.message;
-//   }
-// };
 
 const getappAppStoreById = async (id) => {
   if (!id) {
